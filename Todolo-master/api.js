a6dd0d58f797692a64f376fe7dba7e40128946bb
fjefje <42733276+fjefje@users.noreply.github.com>
2018-11-13 13:05:05 +0100

    Add files via upload
    alt vi har 13.11

---
+++ Todolo-master/api.js
@@ -0,0 +1,7 @@
+/*
+Create user:
+METHOD: POST
+ENDPOINT: api/user
+BODY (json): {"name":"Jonas Magnussen", "email":"jonasmagnussen@gmail.com", "password":"12345678"}
+RESPONS (json): { "id":"123456", "name":"Jonas Magnussen", "email":"jonasmagnussen@gmail.com", "password":"12345678"}
+ */
