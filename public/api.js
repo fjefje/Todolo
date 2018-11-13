e4fbf82a0180dcd4b2c0fdabf805d6184985f591
jonatm15 <31699593+jonatm15@users.noreply.github.com>
2018-11-13 14:24:30 +0100

    Add files via upload

---
+++ public/api.js
@@ -0,0 +1,22 @@
+
+/*
+Create user:
+METHOD: POST
+ENDPOINT: app/student
+BODY (json): {"name":"Jonas Magnussen", "email":"jonasmagnussen@gmail.com", "password":"12345678"}
+RESPONS (json): { "id":"123456", "name":"Jonas Magnussen", "email":"jonasmagnussen@gmail.com", "password":"12345678"}
+
+
+METHOD: GET
+Encoding: UTF-8 (16,32)
+Endpoint: app/student/:ID
+Respons: [{student}]
+
+
+Student:
+{
+    firstName:"string",
+    secondName:"string",
+    ...
+}
+*/
